85277490fb8e928fa51e9fc679f1b92f05ca4408
Tarik Boudraa <boudraa.tariik@gmail.com>
2025-04-25 14:18:24 +0100

    Initialisation du projet

---
+++ app.js
@@ -0,0 +1,18 @@
+const eleves = [
+    { nom: 'Alice', notes: [12, 14, 16] },
+    { nom: 'Bob', notes: [7, 8, 6] },
+    { nom: 'Charlie', notes: [10, 10, 11] }
+];
+// Fonction pour calculer la moyenne d'un élève
+function calculerMoyenne(notes) {
+// À compléter
+}
+// Fonction pour savoir si l'élève est admis (moyenne >= 10)
+function estAdmis(moyenne) {
+// À compléter
+}
+// Fonction pour afficher les résultats
+function afficherResultats() {
+// À compléter
+}
+afficherResultats();
